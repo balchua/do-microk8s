656a967cc781949306d471c49b97223506152e4d
balchua <balchua@yahoo.com>
2020-11-07 07:52:03 +0800

    use smaller droplet size.

--- example/main.tf
+++ example/main.tf
@@ -1,15 +1,15 @@
 module "microk8s" {
-  #source = "git::https://github.com/balchua/do-microk8s"
-  source                       = "../"
-  worker_node_count            = "3"
-  os_image                     = "ubuntu-18-04-x64"
-  controller_size              = "s-4vcpu-8gb"
+  source = "git::https://github.com/balchua/do-microk8s"
+  #source                       = "../"
+  worker_node_count            = "2"
+  os_image                     = "ubuntu-20-04-x64"
+  controller_size              = "s-2vcpu-4gb"
   controller_disksize          = "50"
   worker_disksize              = "50"
   region                       = "sgp1"
-  worker_size                  = "s-4vcpu-8gb"
+  worker_size                  = "s-2vcpu-4gb"
   dns_zone                     = "geeks.sg"
-  microk8s_channel             = "latest/edge/ha-preview"
+  microk8s_channel             = "latest/edge"
   cluster_token                = "PoiuyTrewQasdfghjklMnbvcxz123409"
   cluster_token_ttl_seconds    = 3600
   digitalocean_ssh_fingerprint = var.digitalocean_ssh_fingerprint

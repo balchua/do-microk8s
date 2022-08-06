a67425d84e2f30d646479b85b6121fd5b9db62f3
balchua <balchua@yahoo.com>
2022-08-06 17:07:41 +0800

    add worker node support

---
+++ templates/join-worker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+until microk8s.status --wait-ready; 
+  do sleep 3; echo "waiting for worker status.."; 
+done
+
+
+if microk8s status | grep "datastore master nodes: 127.0.0.1:19001" > /dev/null 2>&1; then
+  echo "adding microk8s-cluster.${dns_zone} dns to CSR."
+  sed -i 's@#MOREIPS@DNS.99 = microk8s-cluster.${dns_zone}\n#MOREIPS\n@g' /var/snap/microk8s/current/certs/csr.conf.template
+  echo "done."
+  sleep 10            
+  microk8s join ${main_node_ip}:25000/${cluster_token} --worker
+else
+  echo "Join process already done. Nothing to do"
+fi
